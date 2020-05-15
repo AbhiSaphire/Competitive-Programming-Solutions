7dc4b03aeb3a126222aa31daec4b1d5f0f0f0fd0
Abhishek Kumar <abhishekkmr2000@gmail.com>
2020-05-16 00:12:12 +0530

    Create ValidParantheses.py

---
+++ LeetCode/ValidParantheses.py
@@ -0,0 +1,5 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        while "()" in s or "{}" in s or '[]' in s:
+            s = s.replace("()", "").replace('{}', "").replace('[]', "")
+        return s == ''

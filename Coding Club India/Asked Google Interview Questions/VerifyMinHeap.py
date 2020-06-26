a7e37a7966be9c0029605f9d1c5a602f016d2928
Abhishek <AbhiSaphire@github.com>
2020-06-26 22:15:02 +0530

    Added Google Interview Questions

---
+++ Coding Club India/Asked Google Interview Questions/VerifyMinHeap.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+	l = [int(x) for x in input().split()]
+	def isValidMaxHeap(l, n):
+		for i in range(((n-2)//2)+1):
+			if l[2*i+1] < l[i]:
+				return False
+			if 2*i+2 < n and l[2*i+2] < l[i]:
+				return False
+		return True
+	print(isValidMaxHeap(l, len(l)))

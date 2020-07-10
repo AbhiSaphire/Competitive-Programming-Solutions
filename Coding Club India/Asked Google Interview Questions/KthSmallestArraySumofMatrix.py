d82e7bc9ed0809167ada0fdd9774e4e84ce37ba1
Abhishek <AbhiSaphire@github.com>
2020-07-10 23:33:23 +0530

    July 10th

--- Coding Club India/Asked Google Interview Questions/KthSmallestArraySumofMatrix.py
+++ Coding Club India/Asked Google Interview Questions/KthSmallestArraySumofMatrix.py
@@ -1,6 +1,8 @@
 # Incomplete solution (Min-Heap Solution) --> O(n) time
+# Completed solution  (Cross Product Rows) --> O(n^2) time [Optimized]
 
 from heapq import heapify, heappush, heappop
+from itertools import product
 
 def KthSmallestArraySumofMatrix(mat, k):
 	heap = []
@@ -10,7 +12,6 @@ def KthSmallestArraySumofMatrix(mat, k):
 			heappush(heap, mat[i][j])
 
 	heapify(heap)
-	print(heap)
 
 	for i in range(k):
 		element = heappop(heap)
@@ -18,8 +19,13 @@ def KthSmallestArraySumofMatrix(mat, k):
 
 	return element
 
+def KthSmallestArraySumofMatrix_NAIVE(mat, k):
+	return sorted([sum(x) for x in product(*mat)])[k-1]
 
-print(KthSmallestArraySumofMatrix([[1, 3, 11], 
+
+print("Incorrect --> ", KthSmallestArraySumofMatrix([	[1, 3, 11], 
 									[2, 4, 6]], 5))
-
 # [3, 5, 5, 7, 7, 9, 13, 15, 17] ---> 5th Smallest Sum --> 7
+
+print("Correct --> ", KthSmallestArraySumofMatrix_NAIVE([[1, 3, 11],
+										[2, 4, 6]], 5))
